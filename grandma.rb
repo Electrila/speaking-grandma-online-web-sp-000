9abeba8342f35401b231e76a2786df5434c0a2db
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-18 13:29:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 def speak_to_grandma(phrase)
   if !phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
-    "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase
+    "NO, NOT SINCE 1938!"
   else
     nil
   end
