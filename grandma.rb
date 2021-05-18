eb97a02257a11b6a1b0000ff53db356b217757f0
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-18 13:27:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,5 +12,13 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
-  if !phrase.
+  if !phrase.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  elsif phrase.upcase
+    "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  else
+    nil
+  end
 end
